94c5f87ad67f8ccfef06057f33b47ae82d876eac
malchak <michaelmalchak@gmail.com>
2013-05-17 21:34:24 -0700

    first commit v.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bootzhibit::Application.config.secret_token = '7f96679f9b43f08f9bd86a3b771de4b74d3cb4d12ce11f70fd811346f51710fb1c585fe051bf79d1b52268816e1812cf5f27c98ec0c46dd538830fc5d869fe18'
