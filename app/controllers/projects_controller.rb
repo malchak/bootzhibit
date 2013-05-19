69c7c1545aca3bfafb69ae6d0c469933b767fad1
malchak <michaelmalchak@gmail.com>
2013-05-19 15:49:33 -0700

    create project dev team display for show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,11 +7,12 @@ class ProjectsController < ApplicationController
 
 	def new
 		@project = Project.new
-		@boots = Boot.new
+		@boot = Boot.new
 	end
 
 	def create
 		@project = Project.create(params[:project])
+		p params
 		redirect_to root_path
 	end
 
